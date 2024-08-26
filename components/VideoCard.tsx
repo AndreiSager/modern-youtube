a9ae93b1049daaae954c0b52f7d5840d82eb2583
Andrei <andreiwork25@gmail.com>
2024-08-26 12:00:00 +0800

    Update VideoCard.tsx

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function VideoCard() {
-  return <div>VideoCard</div>;
+  return (
+    <div>
+      <h1>Lanscape Thumbnail</h1>
+      <div>
+        <h1>Avatar</h1>
+        <h1>Title</h1>
+      </div>
+      <p>Views • Date Published</p>
+    </div>
+  );
 }
