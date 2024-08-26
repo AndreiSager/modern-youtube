aae3a5ea272d7769462bb7050152cd62fd26e0c8
Andrei <andreiwork25@gmail.com>
2024-08-26 12:04:12 +0800

    Update VideoCard.tsx

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function VideoCard() {
   return (
     <div>
-      <h1>Lanscape Thumbnail</h1>
+      <h1>Landscape Thumbnail</h1>
       <div>
         <h1>Avatar</h1>
         <h1>Title</h1>
