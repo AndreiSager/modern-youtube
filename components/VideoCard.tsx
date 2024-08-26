eeb0ff01cbf0274ee87c9665a04510f199298b9e
Andrei <andreiwork25@gmail.com>
2024-08-26 12:07:38 +0800

    Update VideoCard.tsx

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import React from "react";
 
 export default function VideoCard() {
@@ -6,6 +7,10 @@ export default function VideoCard() {
       <h1>Landscape Thumbnail</h1>
       <div>
         <h1>Avatar</h1>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
         <h1>Title</h1>
       </div>
       <p>Views • Date Published</p>
