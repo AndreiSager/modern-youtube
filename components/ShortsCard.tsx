d725fc783cd7991f56c9d425ba5ba0256f1987cb
Andrei <andreiwork25@gmail.com>
2024-08-26 11:58:16 +0800

    Update ShortsCard.tsx

--- components/ShortsCard.tsx
+++ components/ShortsCard.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function ShortsCard() {
-  return <div>ShortsCard</div>;
+  return (
+    <div>
+      <h1>Portrait Thumbnail</h1>
+      <h1>Title</h1>
+      <p>Views</p>
+    </div>
+  );
 }
