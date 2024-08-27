b155b2c55d30a9270cc1501f232bdd1dd7f791d3
Andrei <andreiwork25@gmail.com>
2024-08-27 15:26:18 +0800

    Update SideBar.tsx

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -6,13 +6,57 @@ import { Mail } from "lucide-react";
 export default function SideBar() {
   return (
     <div className=" w-[250px] h-full shadow-md fixed left-0 top-0 pt-14">
-      <ScrollArea className="w-full h-full px-5 flex flex-col items-center justify-center gap-4">
+      <ScrollArea className="w-full h-full pl-5 pr-7 flex flex-col items-center justify-center gap-4">
+        <div className="pt-5">
+          <div className="font-bold text-lg pb-2">Recent</div>
+
+          <div className="w-fit flex flex-row flex-wrap items-end justify-end">
+            {categories.map((category) => (
+              <Button
+                key={category}
+                variant="ghost"
+                className="w-full justify-start"
+              >
+                <Mail className="mr-2 h-4 w-4" /> {category}
+              </Button>
+            ))}
+          </div>
+        </div>
+
+        <Separator className="my-4" />
         <div className="pt-5">
           <div className="font-bold text-lg pb-2">Explore</div>
-          <Button variant="ghost" className="w-full justify-start">
-            <Mail className="mr-2 h-4 w-4" /> Login with Email
-          </Button>
+
+          <div className="w-fit flex flex-row flex-wrap items-end justify-end">
+            {categories.map((category) => (
+              <Button
+                key={category}
+                variant="ghost"
+                className="w-full justify-start"
+              >
+                <Mail className="mr-2 h-4 w-4" /> {category}
+              </Button>
+            ))}
+          </div>
         </div>
+
+        <Separator className="my-4" />
+        <div className="pt-5">
+          <div className="font-bold text-lg pb-2">Explore</div>
+
+          <div className="w-fit flex flex-row flex-wrap items-end justify-end">
+            {categories.map((category) => (
+              <Button
+                key={category}
+                variant="ghost"
+                className="w-full justify-start"
+              >
+                <Mail className="mr-2 h-4 w-4" /> {category}
+              </Button>
+            ))}
+          </div>
+        </div>
+
         <Separator className="my-4" />
         <div className="pb-5 text-xs text-gray-500">
           @2023{" "}
@@ -29,3 +73,13 @@ export default function SideBar() {
     </div>
   );
 }
+
+const categories = [
+  "Trending",
+  "Music",
+  "Moview",
+  "Gaming",
+  "News",
+  "Sports",
+  "Fashion & Beauty",
+];
