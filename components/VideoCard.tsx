64fa08c90793e914e5204f87423859c7091af612
Andrei <andreiwork25@gmail.com>
2024-08-27 11:41:22 +0800

    Update VideoCard.tsx

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -7,14 +7,17 @@ export default function VideoCard() {
       <div className="w-[300px] h-[170px] bg-slate-600 text-white rounded-lg flex items-center justify-center">
         Landscape Thumbnail
       </div>
-      <div className="flex flex-row gap-2 p-2">
+      <div className="flex flex-row gap-2 pt-3">
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
-        <div>
+        <div className="flex-1 mr-8">
           <h1 className="font-semibold">Video Title</h1>
-          <p className="text-sm">Views • Date Published</p>
+          <p className="text-sm">
+            Views
+            <span> • Date Published</span>
+          </p>
         </div>
       </div>
     </div>
