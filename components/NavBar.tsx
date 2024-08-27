2eb771eef047ab2e13d7cf5040b9d8b58fca066d
Andrei <andreiwork25@gmail.com>
2024-08-27 14:35:41 +0800

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,26 @@
+import { Toggle } from "@radix-ui/react-toggle";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Menu } from "lucide-react";
+
 export default function NavBar() {
   return (
-    <div className="w-full h-14 flex items-center justify-between fixed top-0 bg-white p-6">
-      <div>Navbar Left</div>
+    <div className="w-full h-14 flex items-center justify-between fixed top-0 bg-white shadow-md p-6 z-10">
+      <div className="flex justify-center items-center gap-2">
+        <Toggle aria-label="Toggle Sidebar">
+          <Menu className="h-8 w-8" />
+        </Toggle>
+        <h1>Logo</h1>
+        <h1>YouTube</h1>
+      </div>
 
       <div>Navbar Center</div>
 
-      <div>Navbar Right</div>
+      <div>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
     </div>
   );
 }
